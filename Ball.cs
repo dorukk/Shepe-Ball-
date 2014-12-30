60d6091a492276b65e79138aa7f5205b3f45533c
Umur Doruk Kaşoğlu <dorukkasoglu@gmail.com>
2014-12-30 10:55:40 +0200

    Create Ball.cs

---
+++ Ball.cs
@@ -0,0 +1,12 @@
+class Ball
+    {
+        public readonly double Numberpi = 3.14;
+
+        public double area(int r)
+        {
+            double area = Numberpi * r * r;
+            return area;
+        }
+
+
+    }
